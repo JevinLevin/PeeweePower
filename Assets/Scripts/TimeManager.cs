d7473cca1752093f0d9c81cb849025f42ee648de
Jasper Levin <up2106919@myport.ac.uk>
2024-01-22 18:33:02 +0000

    Added Basic Timer

---
+++ Assets/Scripts/TimeManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField] private float startingTime;
+    [SerializeField] private TextMeshProUGUI timerText;
+    [SerializeField] private Image timerBar;
+
+    private float maxTime;
+
+    private float currentTime;
+
+    private void Awake()
+    {
+        maxTime = startingTime;
+    }
+
+    private void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        float t = currentTime / maxTime;
+
+        timerBar.fillAmount = 1 - t;
+
+        int seconds = (int)(maxTime - currentTime);
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
+        string text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText.text = "Time: " + text;
+
+
+    }
+}
